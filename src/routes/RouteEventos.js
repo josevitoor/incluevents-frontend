8b158147664a6a6360278f9913e9bdc2f71be948
josevitoor <josevitor_12@live.com>
2024-05-20 20:45:51 -0300

    adicionando tela de login

--- src/routes/RouteEventos.js
+++ src/routes/RouteEventos.js
@@ -4,17 +4,41 @@ import EventosList from "../components/EventosList";
 import EventosForm from "../components/EventosForm";
 import EventoDetails from "../components/EventoDetails";
 import Validacoes from "../components/Validacoes";
+import Login from "../components/Login";
+import CreateUser from "../components/CreateUser";
+
+const PrivateRoute = ({ element: Element, ...rest }) => {
+  const isAuthenticated = !!localStorage.getItem("token");
+  return isAuthenticated ? <Element {...rest} /> : <Navigate to="/login" />;
+};
 
 const RouteEventos = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/eventos" />} />
-        <Route path="/eventos" index element={<EventosList />} />
-        <Route path="/eventos/new" element={<EventosForm />} />
-        <Route path="/eventos/edit/:id" element={<EventosForm />} />
-        <Route path="/eventos/:id" element={<EventoDetails />} />
-        <Route path="/validacoes" element={<Validacoes />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/create-user" element={<CreateUser />} />
+        <Route
+          path="/eventos"
+          element={<PrivateRoute element={EventosList} />}
+        />
+        <Route
+          path="/eventos/new"
+          element={<PrivateRoute element={EventosForm} />}
+        />
+        <Route
+          path="/eventos/edit/:id"
+          element={<PrivateRoute element={EventosForm} />}
+        />
+        <Route
+          path="/eventos/:id"
+          element={<PrivateRoute element={EventoDetails} />}
+        />
+        <Route
+          path="/validacoes"
+          element={<PrivateRoute element={Validacoes} />}
+        />
       </Routes>
     </BrowserRouter>
   );
